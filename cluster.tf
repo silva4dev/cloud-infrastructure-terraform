a25e6836eab7f609e722c83d8fbdf4f02333b62c
silva4dev <web4dev.lucas@outlook.com>
2024-10-29 21:09:55 -0300

    chore: :sparkles: add role and policies

--- cluster.tf
+++ cluster.tf
@@ -12,5 +12,32 @@ resource "aws_security_group" "sg" {
   }
 }
 
+resource "aws_iam_role" "cluster" {
+  name = "${var.prefix}-${var.cluster_name}-role"
+  assume_role_policy = <<POLICY
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Principal": {
+            "Service": "eks.amazonaws.com"
+          },
+          "Action": "sts:AssumeRole"
+        }
+      ]
+    }
+  POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSVPCResourceController" {
+  role = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+
+
+
+
 
 
