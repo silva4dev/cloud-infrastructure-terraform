f7e75194f86450dcd1047efc393aa512973554b4
silva4dev <web4dev.lucas@outlook.com>
2024-10-31 00:53:04 -0300

    chore: :sparkles: add other node

--- nodes.tf
+++ nodes.tf
@@ -36,6 +36,7 @@ resource "aws_eks_node_group" "node-1" {
   node_group_name = "node-1"
   node_role_arn = aws_iam_role.node.arn
   subnet_ids = aws_subnet.subnets[*].id
+  instance_types = ["t3.micro"]
   scaling_config {
     desired_size = var.desired_size
     max_size = var.max_size
@@ -48,3 +49,20 @@ resource "aws_eks_node_group" "node-1" {
  ]
 }
 
+resource "aws_eks_node_group" "node-2" {
+  cluster_name = aws_eks_cluster.cluster.name
+  node_group_name = "node-2"
+  node_role_arn = aws_iam_role.node.arn
+  subnet_ids = aws_subnet.subnets[*].id
+  instance_types = ["t3.micro"]
+  scaling_config {
+    desired_size = var.desired_size
+    max_size = var.max_size
+    min_size = var.min_size
+  }
+  depends_on = [ 
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
+ ]
+}
